4314f44679dc0ca3a60050c2b3684ceee276e49a
FlexonaFFt <flexonafft@bk.ru>
2024-11-14 17:13:20 +0300

    --load bug fix 2

--- frontend/pages/project/selector.js
+++ frontend/pages/project/selector.js
@@ -3,38 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const timeSeriesBlocks = document.querySelectorAll(
     ".time-series-predictions",
   );
-  const syntheticBlocks = document.querySelectorAll(".synthetic-predictions");
+  const syntheticBlocks = document.querySelectorAll(".synthetic-prediction");
   const radioButtons = document.querySelectorAll(
     'input[name="listGroupCheckableRadios"]',
   );
 
   // Функция для скрытия всех блоков
   function hideAllBlocks() {
-    baseBlocks.forEach((block) => (block.style.visibility = "hidden"));
-    timeSeriesBlocks.forEach((block) => (block.style.visibility = "hidden"));
-    syntheticBlocks.forEach((block) => (block.style.visibility = "hidden"));
+    baseBlocks.forEach((block) => (block.style.display = "none"));
+    timeSeriesBlocks.forEach((block) => (block.style.display = "none"));
+    syntheticBlocks.forEach((block) => (block.style.display = "none"));
   }
 
   radioButtons.forEach((radio) => {
     radio.addEventListener("change", function () {
       hideAllBlocks(); // Сначала скрываем все блоки
 
-      // Затем показываем блоки, соответствующие выбранной радио-кнопке
+      // Затем показываем блоки, соответствующие выбранной радио-кнопке, сбрасывая `display`
       if (this.id === "listGroupCheckableRadios1") {
-        baseBlocks.forEach((block) => (block.style.visibility = "visible"));
+        baseBlocks.forEach((block) => (block.style.display = ""));
       } else if (this.id === "listGroupCheckableRadios2") {
-        timeSeriesBlocks.forEach(
-          (block) => (block.style.visibility = "visible"),
-        );
+        timeSeriesBlocks.forEach((block) => (block.style.display = ""));
       } else if (this.id === "listGroupCheckableRadios3") {
-        syntheticBlocks.forEach(
-          (block) => (block.style.visibility = "visible"),
-        );
+        syntheticBlocks.forEach((block) => (block.style.display = ""));
       }
     });
   });
 
   // Инициализация: скрываем все блоки, кроме базовых
   hideAllBlocks();
-  baseBlocks.forEach((block) => (block.style.visibility = "visible"));
+  baseBlocks.forEach((block) => (block.style.display = ""));
 });
