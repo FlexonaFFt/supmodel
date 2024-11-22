44ac0524c1281a8321e6ea5f0cdbd1df677e0326
FlexonaFFt <flexonafft@bk.ru>
2024-11-22 20:42:40 +0300

    --great dataloading

--- backend/core/dataholder/static/js/scripts/dataloader.js
+++ backend/core/dataholder/static/js/scripts/dataloader.js
@@ -2,14 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("http://127.0.0.1:8000/api/project-data/637206/")
     .then((response) => response.json())
     .then((data) => {
-      // Извлекаем project_name из данных
       const projectName = data.project_name;
-      // Обновляем title страницы
+      const description = data.description;
+      const project_number = data.project_number;
+
       document.getElementById("project-title").textContent = projectName;
+      document.getElementById("main-project-title").textContent = projectName;
+      document.getElementById("main-description").textContent = description;
+      document.getElementById("project-number").textContent =
+        `project #${project_number}`;
     })
     .catch((error) => {
       console.error("Error fetching project data:", error);
       document.getElementById("project-title").textContent =
         "Error loading project data";
+      document.getElementById("main-project-title").textContent =
+        "Error loading project data";
+      document.getElementById("main-description").textContent =
+        "Error loading project description";
+      document.getElementById("project-number").textContent = "project #Error";
     });
 });
