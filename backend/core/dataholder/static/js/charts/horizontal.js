7e22718df3fcc5e4ead7f80afeb3b37c4d41b1d8
FlexonaFFt <flexonafft@bk.ru>
2024-11-24 20:30:39 +0300

    --load linechart

--- backend/core/dataholder/static/js/charts/horizontal.js
+++ backend/core/dataholder/static/js/charts/horizontal.js
@@ -1,77 +1,78 @@
-const ctx = document.getElementById("horizontalChart").getContext("2d");
-const projectNumber = document
-  .getElementById("myChart")
-  .getAttribute("data-project-number");
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("horizontalChart").getContext("2d");
+  const projectNumber = document
+    .getElementById("myChart")
+    .getAttribute("data-project-number");
 
-fetch(`/api/project-data/${projectNumber}/`)
-  .then((response) => response.json())
-  .then((data) => {
-    const userInput = data.user_input;
-    const predictions = data.lstm_time_predictions;
+  fetch(`/api/project-data/${projectNumber}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      const userInput = data.user_input;
+      const predictions = data.lstm_time_predictions;
 
-    const predictedInvestments = predictions.map((pred) =>
-      Math.round(pred.predicted_investments_m),
-    );
-    const predictedCrowdfunding = predictions.map((pred) =>
-      Math.round(pred.predicted_crowdfunding_m),
-    );
+      const predictedInvestments = predictions.map((pred) =>
+        Math.round(pred.predicted_investments_m),
+      );
+      const predictedCrowdfunding = predictions.map((pred) =>
+        Math.round(pred.predicted_crowdfunding_m),
+      );
 
-    const myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Start", ...predictions.map((_, index) => `H${index + 1}`)],
-        datasets: [
-          {
-            label: "Инвестиции",
-            data: [
-              Math.round(userInput.investments_m),
-              ...predictedInvestments,
-            ],
-            backgroundColor: "rgba(255, 99, 132, 0.6)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 1,
-          },
-          {
-            label: "Краудфандинг",
-            data: [
-              Math.round(userInput.crowdfunding_m),
-              ...predictedCrowdfunding,
-            ],
-            backgroundColor: "rgba(54, 162, 235, 0.6)",
-            borderColor: "rgba(54, 162, 235, 1)",
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        indexAxis: "y",
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            beginAtZero: true,
-            title: {
-              display: false,
-              text: "Значения",
+      const myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Start", ...predictions.map((_, index) => `H${index + 1}`)],
+          datasets: [
+            {
+              label: "Инвестиции",
+              data: [
+                Math.round(userInput.investments_m),
+                ...predictedInvestments,
+              ],
+              backgroundColor: "rgba(255, 99, 132, 0.6)",
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 1,
+            },
+            {
+              label: "Краудфандинг",
+              data: [
+                Math.round(userInput.crowdfunding_m),
+                ...predictedCrowdfunding,
+              ],
+              backgroundColor: "rgba(54, 162, 235, 0.6)",
+              borderColor: "rgba(54, 162, 235, 1)",
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          indexAxis: "y",
+          maintainAspectRatio: false,
+          scales: {
+            x: {
+              beginAtZero: true,
+              title: {
+                display: false,
+                text: "Значения",
+              },
+            },
+            y: {
+              title: {
+                display: false,
+                text: "Месяцы",
+              },
             },
           },
-          y: {
-            title: {
+          plugins: {
+            legend: {
               display: false,
-              text: "Месяцы",
+              position: "top",
             },
           },
         },
-        plugins: {
-          legend: {
-            display: false,
-            position: "top",
-          },
-        },
-      },
+      });
     });
-  });
-
+});
 /*
 // Получите элемент canvas
 const ctx = document.getElementById("donutChart2").getContext("2d");
