c62fd2ad691869178e3fc288c1ba837b9a84fa3f
FlexonaFFt <flexonafft@bk.ru>
2024-12-03 21:02:49 +0300

    --load wrkd

--- app/core/myapi/urls.py
+++ app/core/myapi/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import PredictAllFullFormView, GetProjectNumberView
+from .views import PredictAllFullFormView
 
 urlpatterns = [
     path('predict/all_full_form/', PredictAllFullFormView.as_view(), name='predict_all_full_form'),
-    path('project/<int:project_id>/', GetProjectNumberView.as_view(), name='get_project_number'),
 ]
