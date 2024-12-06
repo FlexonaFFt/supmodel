b99211e97cd99e95f1184b49aa36c5b92e0283c4
FlexonaFFt <flexonafft@bk.ru>
2024-12-06 09:47:02 +0300

    --load href-selector

---
+++ app/dataholder/static/js/scripts/href-selector.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("checkoutButton")
+  .addEventListener("click", function () {
+    window.location.href = "http://localhost:8000/checkout";
+  });
+
+document
+  .getElementById("projectlistButton")
+  .addEventListener("click", function () {
+    window.location.href = "http://localhost:8000/project";
+  });
